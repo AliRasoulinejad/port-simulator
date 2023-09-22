92b70aefaf615761225f3e4629f4e8a395790099
ali.rasoulinejad <ali.rasoulinejad75@gmail.com>
2023-09-22 18:37:27 +0330

    improve entities to be usable for simpy

--- entities/vessel.py
+++ entities/vessel.py
@@ -1,6 +1,21 @@
+import random
+import string
+
+from settings.config import Setting
+
+
 class Vessel(object):
-    def __init__(self, *, capacity: int):
+    def __init__(self, *, capacity: int = Setting.vessel.default_capacity):
         """
         :param capacity: The maximum capacity of each vessel
         """
+        self.code = "".join(random.choices(string.digits + string.ascii_letters, k=10))
         self.capacity = capacity
+        self.arrival_time = None
+        self.departure_time = None
+
+    def __str__(self):
+        return self.code
+
+    def __lt__(self, other):
+        return self.arrival_time < other.arrival_time
